b7fe5520c5487f3e5a9a007422feac132911c238
caiosid <caiosidartha8@gmail.com>
2024-05-02 17:54:44 -0300

    Tipos de Variavel no Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //tipos primitivos
+        //estudem a classe String que representa textos
+
+        String meuNome = "Caio Cesar Sidartha Barbosa";
+        
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+        
     }
 }
