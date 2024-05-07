fc744d258347a5f112041cc9f74341f67bc717d7
caiosid <caiosidartha8@gmail.com>
2024-05-07 11:40:20 -0300

    .

--- controle-candidatos/src/candidatura/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/candidatura/ProcessoSeletivo.java
@@ -8,16 +8,31 @@ public class ProcessoSeletivo {
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);*/
 
-
     }
 
+    public void imprimirSelecionados(){
+       String [] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto"};
+          System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+       
+       for(int indice =0; indice < candidatos.length; indice++){
+          System.out.println("O candidato de nº " + (indice + 1) + " é " + candidatos[indice]);
+       }
+
+          System.out.println("Forma abreviada de interação for each");
+
+       for (String candidato : candidatos) {
+          System.out.println("O canditado selecionado foi:" + candidato);
+       }
+    }
+    
+
     public void selecaoCandidatos(){
-        String [] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto","Mônica","Fabricio", "Mirela", "Daniela", "Jorge"};
+        String [] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto","Mônica","Fabricio", "Mirela", "Daniela"};
 
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
         double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.lengthg) {
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
             String candidato = candidatos[candidatosAtual];
             double salarioPretendido = valorPretendido();
 
