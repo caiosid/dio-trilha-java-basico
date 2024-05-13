56c3cc8d4e281aa6d1d0519272ce1feb47894736
caiosid <caiosidartha8@gmail.com>
2024-05-13 10:35:52 -0300

    Exemplo abstração poo

--- msn/src/TelegramMessenger.java
+++ msn/src/TelegramMessenger.java
@@ -1,3 +1,15 @@
 public class TelegramMessenger extends ServicoMensagemInstantanea {
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Telegram Messenger");
+        
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Telegram Messenger");
+        
+    }
    
 }
